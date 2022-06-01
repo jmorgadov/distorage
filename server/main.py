d3d402af3a4c76c677581cf19df4ba587c9e441f
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-06-01 09:58:37 -0400

    Add server basic main script

---
+++ server/main.py
@@ -0,0 +1,34 @@
+# This is just to declare some basic needed actions
+# that a server will do. This might be inplemented
+# in other modules in the future.
+
+
+def discover_servers():
+    raise NotImplementedError()
+
+
+def connect_to_sistem():
+    raise NotImplementedError()
+
+
+def wait_for_clients():
+    raise NotImplementedError()
+
+
+def attend_client():
+    raise NotImplementedError()
+
+
+def wait_for_other_servers():
+    raise NotImplementedError()
+
+
+def main():
+    discover_servers()
+    connect_to_sistem()
+    wait_for_clients()
+    wait_for_other_servers()
+
+
+if __name__ == "__main__":
+    main()
