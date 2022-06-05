f5e7c61a3e53290591b9fe012dae50a65405d320
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-06-05 13:12:52 -0400

    Fix some formatting and typing

--- distorage/server/dht.py
+++ distorage/server/dht.py
@@ -1,52 +1,53 @@
-from operator import le
-from typing import List
+from __future__ import annotations
+
+from typing import List, Optional
+
 
 class ChordNode:
-    
-    def __init__(self, ip, id):
-        self.ip = ip
-        self.id = id
-        self.fingers = [None]*64
-        self.next_finger = 0
-        self.predecessor, self.successor = None, None
-    
-    
-    def find_successor(self, id):
-        if self.id < id <= self.successor.id:
-            return self.successor
-        closet = self.closest_preceding_node(id)
-        return closet.find_successor(id)
-        
+    def __init__(self, ip_addr, node_id: int):
+        self.ip_addr = ip_addr
+        self.node_id: int = node_id
+        self.fingers: List[Optional[int]] = [None] * 64
+        self.next_finger: int = 0
+        self.predecessor: Optional[ChordNode] = None
+        self.successor: Optional[ChordNode] = None
 
-    def closest_preceding_node(self, id):
+    def find_successor(self, node_id: int):
+        # TODO: check what happens if self.successor is None
+        if self.node_id < node_id <= self.successor.node_id:
+            return self.successor
+        closet = self.closest_preceding_node(node_id)
+        return closet.find_successor(node_id)
+
+    def closest_preceding_node(self, node_id: int) -> ChordNode:
         for i in range(len(self.fingers), 0, -1):
             if not self.fingers[i]:
                 continue
-            if self.id < self.fingers[i] < id:
+            # TODO: fingers[i] might be None
+            if self.node_id < self.fingers[i] < node_id:
                 return self.fingers[i]
         return self
-    
-    
-    def join(self, node):
+
+    def join(self, node: ChordNode) -> None:
         self.predecessor = None
-        self.successor = node.find_successor(self.id)
-    
-    
-    def stabilize(self):
+        self.successor = node.find_successor(self.node_id)
+
+    def stabilize(self) -> None:
+        # TODO: check what happens if self.successor is None
         temp = self.successor.predecessor
-        if self.id < temp.id < self.successor.id:
+        if self.node_id < temp.node_id < self.successor.node_id:
             self.successor = temp
         self.successor.notify(self)
-    
-    
-    def notify(self, node):
-        if  not self.predecessor or self.predecessor.id < node.id < self.id:
+
+    def notify(self, node: ChordNode) -> None:
+        if (
+            not self.predecessor
+            or self.predecessor.node_id < node.node_id < self.node_id
+        ):
             self.predecessor = node
-    
-    
-    def fix_fingers():
+
+    def fix_fingers(self):
         raise NotImplementedError()
-    
-    
-    def check_predecessor():
+
+    def check_predecessor(self):
         raise NotImplementedError()
